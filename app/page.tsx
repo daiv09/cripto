126e771a1ea6bf0c9a670e318c1a88e279ebcb0d
Daiwiik Harihar <daiwiikharihar17147@gmail.com>
2025-12-27 02:02:14 +0530

    add coin overview and trending coins ui and working

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
-import Image from "next/image";
-import Link from "next/link";
 
-export default function Home() {
+import TrendingCoins from "@/components/home/TrendingCoins";
+import CoinOverview from "@/components/home/CoinOverview";
+import { Suspense } from "react";
+import {
+  CoinOverviewFallback,
+  TrendingCoinsFallback,
+} from "@/components/home/fallback";
+
+// Adjust type imports to your actual project
+// import type { TrendingCoin, CoinDetailsData } from "@/types";
+
+export default async function Home() {
   return (
-    <div>
-      Page
-    </div>
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+
+      <section className="w-full mt-7 space-y-6">
+        <p>Categories</p>
+      </section>
+    </main>
   );
 }
---
+++ components/home/CoinOverview.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Image from 'next/image'
+import { fetcher } from '@/lib/coingecko.actions'
+import { formatCurrency } from '@/lib/utils'
+
+const CoinOverview = async () => {
+    const coin = await fetcher<CoinDetailsData>('/coins/bitcoin', {
+        dex_pair_format: 'symbol'
+    })
+    return (
+        <div id="coin-overview">
+            <div className="header">
+                <Image
+                    src={coin.image.large}
+                    alt={coin.name}
+                    width={56} height={56}
+                />
+                <div className="info">
+                    <p>{coin.name} / {coin.symbol.toUpperCase()}</p>
+                    <h1>{formatCurrency(coin.market_data.current_price.inr)}</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CoinOverview
