3df92e0a5ed9b2e3e7cde291178e3d623479588e
Daiwiik Harihar <daiwiikharihar17147@gmail.com>
2025-12-27 02:08:36 +0530

    add try catch for data fetch

--- components/home/TrendingCoins.tsx
+++ components/home/TrendingCoins.tsx
@@ -1,89 +1,91 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { TrendingDown, TrendingUp } from 'lucide-react'
-import DataTable from '@/components/DataTable'
-import { cn, formatCurrency } from '@/lib/utils'
-import { fetcher } from '@/lib/coingecko.actions'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { TrendingDown, TrendingUp } from "lucide-react";
+import DataTable from "@/components/DataTable";
+import { cn, formatCurrency } from "@/lib/utils";
+import { fetcher } from "@/lib/coingecko.actions";
 
 const TrendingCoins = async () => {
-    const trendingCoins = await fetcher<{ coins: TrendingCoin[] }>('/search/trending', undefined, 300)
-    type TrendingCoin = {
-        item: {
-            id: string;
-            name: string;
-            large: string;
-            data: {
-                price_change_percentage_24h: { usd: number };
-                price: string;
-            };
-        };
+  let trendingCoins;
+  try {
+    trendingCoins = await fetcher<{ coins: TrendingCoin[] }>(
+      "/search/trending",
+      undefined,
+      300
+    );
+  } catch {
+    trendingCoins = { coins: [] };
+  }
+  type TrendingCoin = {
+    item: {
+      id: string;
+      name: string;
+      large: string;
+      data: {
+        price_change_percentage_24h: { usd: number };
+        price: string;
+      };
     };
-    const columns: DataTableColumn<TrendingCoin>[] = [
-        {
-            header: "Name",
-            cellClassName: "name-cell",
-            cell: (coin) => {
-                const item = coin.item;
-                return (
-                    <Link href={`/coins/${item.id}`}>
-                        <Image
-                            src={item.large}
-                            alt={item.name}
-                            width={32}
-                            height={32}
-                        />
-                        <p>{item.name}</p>
-                    </Link>
-                );
-            },
-        },
-        {
-            header: "24h Change",
-            cellClassName: "change-cell",
-            cell: (coin) => {
-                const item = coin.item;
-                const change = item.data.price_change_percentage_24h.usd;
+  };
+  const columns: DataTableColumn<TrendingCoin>[] = [
+    {
+      header: "Name",
+      cellClassName: "name-cell",
+      cell: (coin) => {
+        const item = coin.item;
+        return (
+          <Link href={`/coins/${item.id}`}>
+            <Image src={item.large} alt={item.name} width={32} height={32} />
+            <p>{item.name}</p>
+          </Link>
+        );
+      },
+    },
+    {
+      header: "24h Change",
+      cellClassName: "change-cell",
+      cell: (coin) => {
+        const item = coin.item;
+        const change = item.data.price_change_percentage_24h.usd;
 
-                const isTrendingUp = change > 0;
-                return (
-                    <div
-                        className={cn(
-                            "price-change",
-                            isTrendingUp ? "text-green-500" : "text-red-500"
-                        )}
-                    >
-                        {isTrendingUp ? (
-                            <TrendingUp width={16} height={16} />
-                        ) : (
-                            <TrendingDown width={16} height={16} />
-                        )}
-                        <span>{Math.abs(change).toFixed(2)}%</span>
-                    </div>
-                );
-            },
-        },
-        {
-            header: "Price",
-            cellClassName: "price-cell",
-            cell: (coin) => coin.item.data.price, // or format as string
-        },
-    ];
-    return (
-        <div id="trending-coins">
-            <h4>Trending Coins</h4>
-            <div id="trending-coins">
-                <DataTable<TrendingCoin>
-                    data={trendingCoins.coins.slice(0, 10)||[]}
-                    columns={columns}
-                    rowKey={(row) => row.item.id}
-                    tableClassName='trending-coins-table'
-                    headerCellClassName='py-3!'
-                    bodyCellClassName='py-2!'
-                />
-            </div>
-        </div>
-    )
-}
+        const isTrendingUp = change > 0;
+        return (
+          <div
+            className={cn(
+              "price-change",
+              isTrendingUp ? "text-green-500" : "text-red-500"
+            )}
+          >
+            {isTrendingUp ? (
+              <TrendingUp width={16} height={16} />
+            ) : (
+              <TrendingDown width={16} height={16} />
+            )}
+            <span>{Math.abs(change).toFixed(2)}%</span>
+          </div>
+        );
+      },
+    },
+    {
+      header: "Price",
+      cellClassName: "price-cell",
+      cell: (coin) => coin.item.data.price, // or format as string
+    },
+  ];
+  return (
+    <div id="trending-coins">
+      <h4>Trending Coins</h4>
+      <DataTable<TrendingCoin>
+        data={trendingCoins.coins.slice(0, 10) || []}
+        columns={columns}
+        rowKey={(row) => row.item.id}
+        tableClassName="trending-coins-table"
+        headerCellClassName="py-3!"
+        bodyCellClassName="py-2!"
+      />
+    </div>
+  );
+};
 
-export default TrendingCoins
+export default TrendingCoins;
